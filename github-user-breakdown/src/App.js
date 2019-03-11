58b1e794b68e8d71b979f262c638ace5047684c2
dislersd <dislersd@gmail.com>
2019-03-11 10:31:30 -0700

    started building login page and authorization

--- github-user-breakdown/src/App.js
+++ github-user-breakdown/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Test from './components/Test';
+import LoginPage from './auth/LoginPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Test />
+      {/* <Test /> */}
+      <LoginPage />
       </div>
     );
   }
