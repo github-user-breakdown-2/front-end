a127eadf215cdf28cf125c9c4b6441b48673d30f
dislersd <dislersd@gmail.com>
2019-03-11 10:14:22 -0700

    set up redux store and started building reducer

--- github-user-breakdown/src/index.js
+++ github-user-breakdown/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
+import rootReducer from './reducers'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,  
+ composeEnhancers(applyMiddleware(thunk, logger))
+ )
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
 
---
+++ github-user-breakdown/src/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+
+export const initialState = {
+  data: [],
+  fetching: false,
+  error: null
+}
+
+const reducer = (state = initialState, action) => {
+switch(action.type) {
+  default: 
+    return state
+  }
+}
+
+export default reducer;
