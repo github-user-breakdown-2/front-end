7eb947afc900092c7f3db1a919c4b9d51034b498
mattr21 <matt.rothstein21@gmail.com>
2019-03-11 10:14:08 -0700

    setup basic reducer. no cases yet

---
+++ github-user-breakdown/src/reducers/index.js
@@ -0,0 +1,20 @@
+// Imported action creators
+
+
+
+// Initial State
+const initialState = {
+    data: ['test'],
+    fetching: false,
+    error: null,
+}
+
+const reducer = (sate = initialState, action) => {
+    switch(action.type) {
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
